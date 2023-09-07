af1b5b5a16814a26ddf6e98b9e39873c0c81a606
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2023-09-07 22:49:21 +0700

    C# lệnh break và continue

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -179,5 +179,19 @@ int day = 2;
 
     }while(a <10);
 
-
 }
+
+// lệnh break  
+    for(int i = 0; i < 10; i++)
+      {
+        if(i ==5) break;
+        System.Console.WriteLine("Vòng lặp thứ {0}",i);
+      }
+// lệnh continue       
+   for(int i = 0; i < 10; i++)
+      {
+        if(i ==5) break;
+        if(i ==5) continue;
+        System.Console.WriteLine("Vòng lặp thứ {0}",i);
+    }
+    
