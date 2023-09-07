0f5cdcbca8c892bd791302f38c90dd03ff4e4d9a
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2023-09-07 22:36:54 +0700

    C# Switch...case

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -147,4 +147,20 @@ internal class Program
         System.Console.WriteLine("Hoc sinh loai Yeu");
       }else {
          System.Console.WriteLine("Hoc sinh loai Kem");
-
+      
+      
+}          
+// cấu trúc rẽ nhánh SWITCH..CASE
+int day = 2;
+    switch(day){ 
+      case 1 : System.Console.WriteLine("Chu nhat"); break;
+      case 2 : System.Console.WriteLine("Thu 2"); break;
+      case 3 : System.Console.WriteLine("Thu 3"); break;
+      case 4 : System.Console.WriteLine("Thu 4"); break;
+      case 5 : System.Console.WriteLine("Thu 5"); break;
+      case 6 : System.Console.WriteLine("Thu 6"); break;
+      case 7 : System.Console.WriteLine("Thu 7"); break;
+      default :
+        System.Console.WriteLine("Khong phai ngay trong tuan");
+        break;
+    }  
