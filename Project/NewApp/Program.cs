3e07c95832a53b7c2d77fbdb3481a557a9f740eb
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2023-09-07 22:28:18 +0700

    C# if..else

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -134,3 +134,17 @@ internal class Program
             Console.WriteLine("{0} lon hon {1}",a, b);
     }
 }
+
+// ví dụ cấu trúc rẽ nhánh if...esle
+  float a =1;
+      if(a >=8.5){ 
+        System.Console.WriteLine("Hoc sinh dat loai gioi");
+      } else if(a >=6.5){
+        System.Console.WriteLine("Hoc sinh dat loai Kha");
+      } else if(a>=5){
+        System.Console.WriteLine("Hoc sinh loai Trung Binh");
+      }else if(a>=3.5){
+        System.Console.WriteLine("Hoc sinh loai Yeu");
+      }else {
+         System.Console.WriteLine("Hoc sinh loai Kem");
+
