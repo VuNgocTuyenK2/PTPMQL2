5d399835aae96129648cd208d8d19a9370650962
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2023-09-26 21:49:02 +0700

    khoi tao person

--- Project/NewApp/Program.cs
+++ Project/NewApp/Program.cs
@@ -1,8 +1,15 @@
 ﻿using NewApp.Models;
+public class Program
+{
+    private static void Main(string[] args)
+    {
+
 
 Person ps = new Person();
+Person ps2 = new Person();
 ps.NhapThongTin();
 ps.HienThi();
+ps2.HienThi();
 //vu ngoc tuyen 2021050715
 Employee emp = new Employee();
 emp.NhapThongTin();
@@ -14,3 +21,6 @@ std.HienThi();
 Fruit frt = new Fruit();
 frt.NhapThongTin();
 frt.HienThi();
+    }
+
+}
