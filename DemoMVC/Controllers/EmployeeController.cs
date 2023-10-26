1d0600b78f4e69a62faa495cbfb4afbba79b7a58
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2023-10-26 19:50:48 +0700

    slide 17 EmployeeMVC

---
+++ DemoMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,21 @@
+using DemoMVC.Models;
+using Microsoft.AspNetCore.Mvc;
+namespace DemoMVC.Controllers;
+
+    public class EmployeeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Index(Employee emp)
+        {
+            string strResult = "Hello " + emp.EmployeeID + " - " + emp.FullName;
+            ViewBag.noti = strResult;
+            return View();
+        }
+    }
+
+
---
+++ DemoMVC/Models/Employee.cs
@@ -0,0 +1,7 @@
+namespace DemoMVC.Models;
+
+public class Employee
+{
+    public int EmployeeID { get; set; }
+    public string FullName { get; set; }
+}
