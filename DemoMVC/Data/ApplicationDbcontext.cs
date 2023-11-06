417de43979fdd449a02c93d4a643b5dbbd9908f0
VuNgocTuyenK2 <tuyeni02km2@gmail.com>
2023-11-06 14:26:24 +0700

    slide 67 Controller va View class Employee

--- DemoMVC/Data/ApplicationDbcontext.cs
+++ DemoMVC/Data/ApplicationDbcontext.cs
@@ -8,6 +8,7 @@ namespace DemoMVC.Data
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
         {}
         public DbSet <Person> Person { get; set; }
+        public DbSet<Employee> Employee { get; set; } = default!;
         
         
     }
